4b10ce3d95ab622b875182dfa679af17a0024fa0
Jewel M <jeweltmichael@yahoo.com>
2022-02-06 23:38:05 +0530

    menu animation

--- src/Component/Layout/Content/Motions.ts
+++ src/Component/Layout/Content/Motions.ts
@@ -3,3 +3,29 @@ export const pageMotion = {
   animate: { opacity: 1, y: 0, transition: { duration: 0.2 } },
   exit: { opacity: 0, y: 5, transition: { duration: 0.2 } }
 };
+export const sideVariants = {
+  closed: {
+    height: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+      when: "afterChildren",
+      staggerDirection: -1
+    }
+  },
+  open: {
+    height: '100vh',
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+      when: "beforeChildren",
+      staggerDirection: 1
+    }
+  }
+};
+export const itemVariants = {
+  closed: {
+    opacity: 0
+  },
+  open: { opacity: 1 }
+};
--- src/Component/Layout/Header/Header.tsx
+++ src/Component/Layout/Header/Header.tsx
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import s from './Header.module.scss';
+import MenuToggle from '../../MenuToggle/MenuToggle';
+import { AnimatePresence, motion, useCycle } from "framer-motion";
+import { sideVariants, itemVariants } from "../Content/Motions";
 
 function Header() {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    setShow(show => !show);
+  };
   return (
     <div className={s.header}>
       <div className="container">
         <div className={s.wrapper}>
           <Link to="/" className={s.logo}>Jewel Michael</Link>
           <div className={s.navContainer}>
-            <nav className={s.navBlock}>
-              <Link to="/" className={s.navItem}>Home</Link>
-              <Link to="/about" className={s.navItem}>about</Link>
-              <Link to="/works" className={s.navItem}>work</Link>
-              <Link to="/contact" className={s.navItem}>contact</Link>
-            </nav>
+
+            <MenuToggle onClick={handleClick} show={show} />
+            <AnimatePresence>
+              {show &&
+                <motion.div
+                  className="navWrapper"
+                >
+                  <motion.nav
+                    className={s.navBlock}
+                    initial="closed"
+                    animate="open"
+                    exit="closed"
+                    variants={sideVariants}
+                  >
+                    <motion.div
+                      whileHover={{ scale: 1.1 }}
+                      variants={itemVariants}
+                      className={s.navItem}
+                    >
+                      <Link to="/">Home</Link>
+                    </motion.div>
+                    <motion.div
+                      whileHover={{ scale: 1.1 }}
+                      variants={itemVariants}
+                      className={s.navItem}
+                    >
+                      <Link to="/about">about</Link>
+                    </motion.div>
+                    <motion.div
+                      whileHover={{ scale: 1.1 }}
+                      variants={itemVariants}
+                      className={s.navItem}
+                    >
+                      <Link to="/works">work</Link>
+                    </motion.div>
+                    <motion.div
+                      whileHover={{ scale: 1.1 }}
+                      variants={itemVariants}
+                      className={s.navItem}
+                    >
+                      <Link to="/contact">contact</Link>
+                    </motion.div>
+                  </motion.nav>
+                </motion.div>
+              }
+            </AnimatePresence>
           </div>
         </div>
       </div>
